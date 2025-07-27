e64520c5d5db7e1035e5437f5c663c1ebad340d8
Steven L <steven.s.llamas@gmail.com>
2025-07-27 16:15:04 -0500

    added icon

--- src/hl7/gui/frame.java
+++ src/hl7/gui/frame.java
@@ -1,17 +1,20 @@
 package hl7.gui;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.net.URL;
 
 public class frame {
 
     public frame(){
         var frame = new JFrame("HL7 Parser");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
-
-
-        frame.add(label);
+        frame.setResizable(false);
+        frame.setSize(700, 700);
         frame.setVisible(true);
 
+        URL iconURL = getClass().getResource("/hl7/images/important.jpg");
+        var image = new ImageIcon(iconURL);
+        frame.setIconImage(image.getImage());
     }
 }
